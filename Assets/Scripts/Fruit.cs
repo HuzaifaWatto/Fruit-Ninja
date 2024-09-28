caf57058dff670954398c8a55b94717b4fcfed4f
HuzaifaWatto <98208958+HuzaifaWatto@users.noreply.github.com>
2024-09-28 15:16:18 +0500

    Add files via upload

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject whole;
+    public GameObject sliced;
+
+    private Rigidbody fruitRigidbody;
+    private Collider fruitCollider;
+    private ParticleSystem juiceEffect;
+
+    public int points = 1;
+
+    private void Awake()
+    {
+        fruitRigidbody = GetComponent<Rigidbody>();
+        fruitCollider = GetComponent<Collider>();
+        juiceEffect = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void Slice(Vector3 direction, Vector3 position, float force)
+    {
+        FindObjectOfType<GameManager>().IncreaseScore(points);
+
+        // Disable the whole fruit
+        fruitCollider.enabled = false;
+        whole.SetActive(false);
+
+        // Enable the sliced fruit
+        sliced.SetActive(true);
+        juiceEffect.Play();
+
+        // Rotate based on the slice angle
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        sliced.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
+
+        // Add a force to each slice based on the blade direction
+        foreach (Rigidbody slice in slices)
+        {
+            slice.velocity = fruitRigidbody.velocity;
+            slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Blade blade = other.GetComponent<Blade>();
+            Slice(blade.direction, blade.transform.position, blade.sliceForce);
+        }
+    }
+
+}
